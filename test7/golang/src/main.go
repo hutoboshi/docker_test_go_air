de48a165c0e76ff9ce5469b05e5a7949eba84739
taisei <hutoboshi.eng@gmail.com>
2024-09-28 17:40:04 +0900

    test7

---
+++ test7/golang/src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"test7/article"
+	"time"
+)
+
+func open(path string, count uint) *sql.DB {
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		log.Fatal("open error:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		time.Sleep(time.Second * 2)
+		count--
+		fmt.Println("retry... count:%v\n", count)
+		return open(path, count)
+	}
+
+	fmt.Println("db connected!!")
+	return db
+}
+
+func connectedDB() *sql.DB {
+	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
+		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+	return open(path, 100)
+}
+
+func main() {
+	db := connectedDB()
+	defer db.Close()
+	article.ReadAll(db)
+}
